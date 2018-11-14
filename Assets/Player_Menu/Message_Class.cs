13f829f3abc42391a88a2d324410b068dd46e381
Kozjar <qwertzxc0000@gmail.com>
2018-11-14 19:24:11 +0300

    Message Menu

--- Assets/Player_Menu/Message_Class.cs
+++ Assets/Player_Menu/Message_Class.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Message_Class : MonoBehaviour {
 
@@ -12,15 +13,41 @@ public static class MessageInventory
 }
 
 
+[System.Serializable]
 public class Message
 {
     public GameObject Prefab;
+    public Transform _Content;
+    public GameObject FullTextArea;
+
+    [TextArea]
     public string Text_Message;
     public Sprite Miniature;
 
     public void AddThisMessageToInventory()
     {
         MessageInventory.MessageStack.Add(this);
-        //GameObject.Instantiate();
+        GameObject Clone;
+        Clone = GameObject.Instantiate(Prefab, _Content);
+        Clone.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = Miniature;
+        Clone.transform.GetChild(1).GetComponent<Text>().text = GetMiniText();
+        Clone.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(delegate { FullTextArea.SetActive(true); });
+        Clone.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(delegate { ShowMessage(Text_Message); });
+    }
+
+    public void ShowMessage(string text)
+    {
+        FullTextArea.transform.GetChild(0).GetComponent<Text>().text = text;
+    }
+
+    public string GetMiniText()
+    {
+        string _Text;
+        int x = 52;
+        if (Text_Message.Length < x)
+            _Text = Text_Message.ToString();
+        else
+            _Text = Text_Message.Substring(0, x) + "...";
+        return _Text;
     }
 }
---
+++ Assets/Player_Menu/Message_Empty.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message_Empty : MonoBehaviour {
+
+    public Message ThisMessage;
+
+    public void AddThisMessage()
+    {
+        ThisMessage.AddThisMessageToInventory();
+    }
+}
