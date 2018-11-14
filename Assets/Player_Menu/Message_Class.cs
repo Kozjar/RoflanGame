1025a8c0965df8ffe55a9b3b904f85e8f7a35ff7
Kozjar <qwertzxc0000@gmail.com>
2018-11-14 20:36:57 +0300

    With Comments

--- Assets/Player_Menu/Message_Class.cs
+++ Assets/Player_Menu/Message_Class.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 public class Message_Class : MonoBehaviour {
     public MessageSettingsAnother set;
 
+    //Есть экземпляр класса настроек, который прикрепляется к пустому объекту на сцене.
+    //В инспекторе мы указываем все необходимые настройки, которые будут применяться ко всем письмам.
+    //Потом приравниваем все поля такого же, но статического класса к полям этого экземпляра.
+    //Все это делается, чтобы не пришлось для каждого письма рачками в инспекторе указывать одни и те же панель меню, префаб и т.д.
+
     private void Start()
     {
         MessageSettingsAnotherStatic.FullTextArea = set.FullTextArea;
@@ -14,14 +19,19 @@ public class Message_Class : MonoBehaviour {
     }
 }
 
-[CreateAssetMenu(fileName = "MessageSettings", menuName = "MessageSettings")]
-public class MessageSettings : ScriptableObject
-{
-    public GameObject Prefab;
-    public Transform _Content;
-    public GameObject FullTextArea;
-}
 
+/////////////////////Пока еще разбираюсь с этим//////////////////////////////////
+//[CreateAssetMenu(fileName = "MessageSettings", menuName = "MessageSettings")]//
+//public class MessageSettings : ScriptableObject                              //
+//{                                                                            //
+//    public GameObject Prefab;                                                //
+//    public Transform _Content;                                               //
+//    public GameObject FullTextArea;                                          //
+//}                                                                            //
+/////////////////////////////////////////////////////////////////////////////////
+
+
+//Класс настроек
 [System.Serializable]
 public class MessageSettingsAnother
 {
@@ -30,6 +40,7 @@ public class MessageSettingsAnother
     public GameObject FullTextArea;
 }
 
+//Статический класс настроек, который используется для всех писем
 public static class MessageSettingsAnotherStatic
 {
     public static GameObject Prefab;
@@ -37,6 +48,7 @@ public static class MessageSettingsAnotherStatic
     public static GameObject FullTextArea;
 }
 
+//Коллекция наших писем для будущей базы данных
 public static class MessageInventory
 {
     public static List<Message> MessageStack = new List<Message>();
@@ -48,33 +60,38 @@ public class Message
 {
 
     [TextArea]
-    public string Text_Message;
-    public Sprite Miniature;
+    public string Text_Message; //Текст письма
+    public Sprite Miniature; //миниатюра, которая будет отображаться в инвентаре
 
+    //Добавляет письмо в инвентарь
     public void AddThisMessageToInventory()
     {
-            MessageInventory.MessageStack.Add(this);
+            MessageInventory.MessageStack.Add(this); //Добавляем новое письмо в коллекцию писем
             GameObject Clone;
-            Clone = GameObject.Instantiate(MessageSettingsAnotherStatic.Prefab, MessageSettingsAnotherStatic._Content);
-            Clone.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = Miniature;
-            Clone.transform.GetChild(1).GetComponent<Text>().text = GetMiniText();
-            Clone.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(delegate { MessageSettingsAnotherStatic.FullTextArea.SetActive(true); });
-            Clone.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(delegate { ShowMessage(Text_Message); });
+            Clone = GameObject.Instantiate(MessageSettingsAnotherStatic.Prefab, MessageSettingsAnotherStatic._Content); //создаем пустую панель письма в меню писем
+            Clone.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = Miniature; //Задаем миниатюру письма
+            Clone.transform.GetChild(1).GetComponent<Text>().text = GetMiniText(); //Задаем краткий текст
+            Clone.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(delegate { ShowMessage(Text_Message); }); //Задаем инструкции кнопки "Read"
     }
 
+    //Показать полное письмо
     public void ShowMessage(string text)
     {
-            MessageSettingsAnotherStatic.FullTextArea.transform.GetChild(0).GetComponent<Text>().text = text;
+        MessageSettingsAnotherStatic.FullTextArea.transform.GetChild(0).GetComponent<Text>().text = text;
+        MessageSettingsAnotherStatic.FullTextArea.SetActive(true);
     }
 
+    //Функция, которая возвращает сокращенный текст письма
     public string GetMiniText()
     {
-        string _Text;
-        int x = 52;
+        string _Text; //Сокращенный текст письма
+        int x = 52; //Колличество букв, которые будут показываться в сокращенном тексте письма
+
         if (Text_Message.Length < x)
             _Text = Text_Message.ToString();
         else
             _Text = Text_Message.Substring(0, x) + "...";
+
         return _Text;
     }
 }
