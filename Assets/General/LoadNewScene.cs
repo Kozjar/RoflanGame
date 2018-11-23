711faf34d49914a95acb92b74f590ec79d9787c9
Kozjar <qwertzxc0000@gmail.com>
2018-11-23 18:32:31 +0300

    Пофиксил перенос инвентаря

--- Assets/General/LoadNewScene.cs
+++ Assets/General/LoadNewScene.cs
@@ -7,14 +7,20 @@ public class LoadNewScene : MonoBehaviour {
 	public GameObject Inv;
 	public GameObject player;
 	public GameObject Staticdata;
-	void OnTriggerEnter2D(Collider2D collider)
+    public GameObject EventSystem;
+    private void Start()
+    {
+        DontDestroyOnLoad(Inv);
+        DontDestroyOnLoad(player);
+        DontDestroyOnLoad(Staticdata);
+        DontDestroyOnLoad(EventSystem);
+    }
+    void OnTriggerEnter2D(Collider2D collider)
 	{
 		if (collider.tag == "Player")
 		{
-			DontDestroyOnLoad(Inv);
-			DontDestroyOnLoad(player);
-			DontDestroyOnLoad(Staticdata);
-			SceneManager.LoadScene(2);
+
+            SceneManager.LoadScene(2);
 		}
 	}
 		
