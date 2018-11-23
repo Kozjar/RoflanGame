ac1513a5e887d69eb70fa57ead278da2f9b6d3c5
Kozjar <qwertzxc0000@gmail.com>
2018-11-23 18:08:00 +0300

    Микрофикс

---
+++ Assets/General/Merchant_Dialog_Behavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Merchant_Dialog_Behavior : MonoBehaviour {
+    public Merchant_Dialog Merchant_Dialog;
+
+    public void StartDialog()
+    {
+        Merchant_Dialog.startDialog();
+    }
+
+}
+[System.Serializable]
+public class Merchant_Dialog : NPCDialog
+{
+    public override void startDialog()
+    {
+        base.startDialog();
+    }
+}
