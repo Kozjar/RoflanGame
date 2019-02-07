564e40e6dc7ea7da13ea1beb3661f5480abee28f
Kozjar <qwertzxc0000@gmail.com>
2019-02-07 18:59:30 +0300

    aa

--- Assets/Scripts/Layers_System/Z_Pos_Start.cs
+++ Assets/Scripts/Layers_System/Z_Pos_Start.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,21 +14,3 @@ public class Z_Pos_Start : MonoBehaviour {
 		
 	}
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Z_Pos_Start : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
-    }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
->>>>>>> 6c82bedcb85dbd537d4ce795f0d49c349522338a
